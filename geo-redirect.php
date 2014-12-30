4fe09548bbf0f8bc19d38d84ab42f78a7a2aa9e4
Ivan Denkov <ivan.denkov87@gmail.com>
2014-12-30 02:18:54 +0200

    Improvments

--- geo-redirect.php
+++ geo-redirect.php
@@ -32,9 +32,9 @@
   $iplist = file_get_contents($ipfile);
 //File size
   $size = filesize($ipfile);
-//echo $ipfile;
-//echo $iplist;
-//echo $ipfile . " is " . $size . " bytes.";
+  //echo $ipfile;
+  //echo $iplist;
+  //echo $ipfile . " is " . $size . " bytes.";
 
 //Adding the clinet IP to the cache file
 file_put_contents($ipfile, $ipc . PHP_EOL, FILE_APPEND | LOCK_EX);
@@ -44,34 +44,33 @@ file_put_contents($ipfile, $ipc . PHP_EOL, FILE_APPEND | LOCK_EX);
     file_put_contents($ipfile, "");
     }
 
-    function register_mysettings() {
+    function register_geosettings() {
       //register our settings
-      register_setting( 'baw-settings-group', 'new_option_name' );
-      register_setting( 'baw-settings-group', 'some_other_option' );
-      register_setting( 'baw-settings-group', 'option_etc' );
+      register_setting( 'grm-settings-group', 'new_option_name' );
+      register_setting( 'grm-settings-group', 'some_other_option' );
+      register_setting( 'grm-settings-group', 'option_etc' );
     }
 
-    function settings_page() {
+    function geo_settings_page() {
       ?>
       <div class="wrap">
-        <h2>Your Plugin Name</h2>
-
+        <h2>Geo Redirection</h2>
         <form method="post" action="options.php">
-          <?php settings_fields( 'baw-settings-group' ); ?>
-          <?php do_settings_sections( 'baw-settings-group' ); ?>
+          <?php settings_fields( 'grm-settings-group' ); ?>
+          <?php do_settings_sections( 'grm-settings-group' ); ?>
           <table class="form-table">
             <tr valign="top">
-              <th scope="row">New Option Name</th>
+              <th scope="row">New Option Name|Your IP</th>
               <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
             </tr>
 
             <tr valign="top">
-              <th scope="row">Some Other Option</th>
+              <th scope="row">Some Other Option|MaxMind URL</th>
               <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
             </tr>
 
             <tr valign="top">
-              <th scope="row">Options, Etc.</th>
+              <th scope="row">Options, Etc.|MaxMind Credentials</th>
               <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
             </tr>
           </table>
@@ -82,11 +81,10 @@ file_put_contents($ipfile, $ipc . PHP_EOL, FILE_APPEND | LOCK_EX);
       </div>
       <?php }
 
-
   function geo_plugin_options(){
-    add_menu_page('Geo Plugin Settings', 'Geo Redirect Settings', 'manage_options', 'geolocation-settings', 'settings_page', 'dashicons-networking');
+    add_menu_page('Geo Plugin Settings', 'Geo Redirect Settings', 'manage_options', 'geolocation-settings', 'geo_settings_page', 'dashicons-networking');
   }
 
   //call register settings function
-  add_action( 'admin_init', 'register_mysettings' );
+  add_action( 'admin_init', 'register_geosettings' );
   add_action('admin_menu', 'geo_plugin_options');
